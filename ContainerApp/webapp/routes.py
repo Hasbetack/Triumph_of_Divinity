33703e9abba223ab1906abd496761ff7b3df5e87
HelplessEskimo <helpless.eskimo@fuckyou.net>
2021-08-27 22:27:45 +0100

    minor refactors

--- ContainerApp/webapp/routes.py
+++ ContainerApp/webapp/routes.py
@@ -1,33 +1,38 @@
 from flask import render_template, url_for, flash, redirect, request
 from webapp import app
-from webapp.utils import load_changelog_json, load_datacard_json, filter_datacards_by_faction, filter_datacards_by_keyword, load_datacards_from_files
+from webapp.utils import load_changelog_json, filter_datacards_by_faction, filter_datacards_by_keyword, load_datacards_from_files
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('index.html')
 
+
 @app.route("/datacards", methods=['GET', 'POST'])
 def datacards():
     datacards_data, all_factions, all_keywords = load_datacards_from_files()
-    
+
     if request.method == 'POST':
         faction_list = request.form.getlist('faction-checkbox')
         keyword_list = request.form.getlist('keyword-checkbox')
         datacards_data = filter_datacards_by_faction(datacards_data, faction_list)
         datacards_data = filter_datacards_by_keyword(datacards_data, keyword_list, False)
 
-    return render_template('datacards.html', datacards = datacards_data, all_factions = all_factions, all_keywords = all_keywords)
+    return render_template('datacards.html', datacards=datacards_data, all_factions=all_factions, all_keywords=all_keywords)
+
 
 @app.route("/rules")
 def rules():
     return render_template('rules.html')
 
+
 @app.route("/army_builder")
 def army_builder():
     return render_template('army_builder.html')
 
+
 @app.route("/changelog")
 def changelog():
     changelog_data = load_changelog_json()
-    return render_template('changelog.html', changelog = changelog_data)
+    return render_template('changelog.html', changelog=changelog_data)
--- ContainerApp/webapp/utils.py
+++ ContainerApp/webapp/utils.py
@@ -8,15 +8,15 @@ def get_large_json(path, faction_name, json_dir):
     if len(os.listdir(path + faction_name + json_dir)) == 0:
         return []
 
-    companies = []
+    list_of_cards = []
 
-    company_files = [pos_json for pos_json in os.listdir(path + faction_name + json_dir) if pos_json.endswith('.json')]
-    for index, js in enumerate(company_files):
-        with open(os.path.join(path + faction_name + json_dir, js), encoding="utf8") as company_file:
-            companies_dict = json.load(company_file)
-            companies.append(companies_dict)
+    json_files = [pos_json for pos_json in os.listdir(path + faction_name + json_dir) if pos_json.endswith('.json')]
+    for index, js in enumerate(json_files):
+        with open(os.path.join(path + faction_name + json_dir, js), encoding="utf8") as json_file:
+            card_dict = json.load(json_file)
+            list_of_cards.append(card_dict)
 
-    return companies
+    return list_of_cards
 
 
 def get_small_json(path, faction_name, json_dir, filename):
@@ -24,10 +24,10 @@ def get_small_json(path, faction_name, json_dir, filename):
     if len(os.listdir(path + faction_name + json_dir)) == 0:
         return {}
 
-    with open(os.path.join(path + faction_name + json_dir + filename), "r", encoding="utf8") as ability_file:
-        abilities = json.load(ability_file)
+    with open(os.path.join(path + faction_name + json_dir + filename), "r", encoding="utf8") as json_file:
+        card_dict = json.load(json_file)
 
-    return abilities
+    return card_dict
 
 
 def construct_factions_dict():
@@ -54,139 +54,6 @@ def load_datacards_from_files():
     factions = construct_factions_dict()
     return factions, factions.keys(), ["Infantry", "Cavalry", "Character", 'Magistro Malitiae', "Elite", "Legendary", "Line Troop"]
 
-
-# TODO old function, deprecate when possible
-def load_datacard_json():
-    # Create something like this from saved JSON
-    # Return the datastructure, a list of all factions in it, a list of all keywords
-    datacards = {
-        'Greeks': {
-            'Abilities': {
-            },
-            'Companies': [
-                {
-                    'Name': 'The Gates of the Underworld',
-                    'Points': 600,
-                    'Requirements': ['Requirement 1', 'Requirement 2', 'Requirement 3'],
-                    'Effect': 'Effect description'
-                },
-                {
-                    'Name': 'The Gift of Greed',
-                    'Points': 200,
-                    'Requirements': ['Requirement 1', 'Requirement 2'],
-                    'Effect': 'Effect description 2'
-                }
-            ],
-            'Units': [
-                {
-                    'Name': 'Poseidon',
-                    'Keywords': ['Character', 'Legendary'],
-                    'Points': 2700,
-                    'Unit_Size': {'min': 1, 'max': 1},
-                    'Move': 12,
-                    'Dash': 7,
-                    'Faction_Abilities': [],
-                    'Unit_Abilities': [],
-                    # etc...
-                    'Caster': 3,
-                    'Spells': [
-                        {
-                            'Name': 'Rage of the Ocean (35)',
-                            'Effect': 'Select D3 enemy formations within 18” of the caster. Then roll 2D6 for each model in each formation (to a maximum of 15 D6). For each 4+ that model’s formation suffers 1 ethereal damage.'
-                        }   
-                    ]                   
-                },
-                {
-                    'Name': 'Hoplites',
-                    'Keywords': ['Infantry', 'Line Troop'],
-                    'Points': 20,
-                    'Unit_Size': {'min': 10, 'max': 20},
-                    'Move': 5,
-                    'Dash': 4,
-                    'Faction_Abilities': [],
-                    'Unit_Abilities': []
-                    # etc...
-                }
-            ]
-        },
-        'Romans': {
-            'Abilities': {
-                'Servants of the Emperor': 'While a friendly <span class="datacard-keyword">MAGISTRO MALITAE CHARACTER</span> is within 6" of this formation, then models in this formation cannot fail Will tests.'
-            },
-            'Companies': [
-                {
-                    'Name': 'The Chosen Sons',
-                    'Points': 500,
-                    'Requirements': ['Requirement 1', 'Requirement 2', 'Requirement 3'],
-                    'Effect': 'Effect description'
-                },
-                {
-                    'Name': 'The Eternal Wall',
-                    'Points': 200,
-                    'Requirements': ['Requirement 1', 'Requirement 2'],
-                    'Effect': 'Effect description 2'
-                }
-            ],
-            'Units': [
-                {
-                    'Name': 'Scholae Palatine',
-                    'Keywords': ['Cavalry', 'Elite', 'Magistro Malitiae'],
-                    'Points': 90,
-                    'Unit_Size': {'min': 3, 'max': 6},
-                    'Move': 12,
-                    'Dash': 6,
-                    # etc...
-                    'Weapons': [
-                        {
-                            'Cost': 0,
-                            'Name': 'blah',
-                            'Type': 'blah',
-                            'Range': 'blah',
-                            'Wound': 'blah',
-                            'Rend': 'blah',
-                            'D': 'blah',
-                            'Abilities': 'blah'
-                        },
-                        {
-                            'Cost': 0,
-                            'Name': 'blah',
-                            'Type': 'blah',
-                            'Range': 'blah',
-                            'Wound': 'blah',
-                            'Rend': 'blah',
-                            'D': 'blah',
-                            'Abilities': 'blah'
-                        }
-                    ],
-                    'Faction_Abilities': ['Servants of the Emperor'],
-                    'Unit_Abilities': [
-                        {
-                            'Ability_Name': "The Emperor's Bodyguard",
-                            'Ability_Effect': 'While a friendly <span class="datacard-keyword">MAGISTRO MALITIAE CHARACTER</span> is within 3" of this formation then that character cannot be selected as the target of ranged attacks.'
-                        },
-                        {
-                            'Ability_Name': 'Militiae Battle Shield',
-                            'Ability_Effect': 'Improve the Armour of models in this formation by 1.'
-                        }
-                    ],
-                    'Caster': False,
-                    'Spells': []
-                },
-                {
-                    'Name': 'Centurion',
-                    'Keywords': ['Infantry', 'Character', 'Magistro Malitiae'],
-                    'Points': 240,
-                    'Unit_Size': {'min': 1, 'max': 1},
-                    'Move': 6,
-                    'Dash': 4,
-                    'Faction_Abilities': [],
-                    'Unit_Abilities': []
-                    # etc...
-                }
-            ]
-        }
-    }
-    return datacards, datacards.keys(), ["Infantry", "Cavalry", "Character", 'Magistro Malitiae', "Elite", "Legendary", "Line Troop"]
 
 def load_changelog_json():
     # Create something like this from saved JSON
@@ -215,6 +82,7 @@ def load_changelog_json():
     ]
     return changelog
 
+
 def filter_datacards_by_faction(datacards, searched_factions):
     """
     Returns only datacards that belong to one of the specified
@@ -224,24 +92,27 @@ def filter_datacards_by_faction(datacards, searched_factions):
     datacards:          datastructure containing all datacards.
     searched_factions : list containing faction name keywords searched for.
     """
-    assert type(searched_factions) == list, "seached_factions must be a list"
+    assert type(searched_factions) == list, "searched_factions must be a list"
     return {k: v for k, v in datacards.items() if k in searched_factions}
+
 
 def all_keywords_shared(datacard_keywords, searched_keywords):
     datacard_set = set(datacard_keywords)
     searched_set = set(searched_keywords)
     if (datacard_set | searched_set) == datacard_set:
-        return True 
+        return True
     else:
         return False
+
 
 def any_keywords_shared(datacard_keywords, searched_keywords):
     datacard_set = set(datacard_keywords)
     searched_set = set(searched_keywords)
     if (datacard_set & searched_set):
-        return True 
+        return True
     else:
         return False
+
 
 def filter_datacards_by_keyword(datacards, searched_keywords, strict):
     """
@@ -254,7 +125,7 @@ def filter_datacards_by_keyword(datacards, searched_keywords, strict):
     searched_keywords: list containing unit keywords searched for.
     strict (bool):     if True all searched_keywords must be present on the datacard, otherwise at least one must be present.
     """
-    assert type(searched_keywords) == list, "seached_keywords must be a list"
+    assert type(searched_keywords) == list, "searched_keywords must be a list"
     for faction in datacards.keys():
         if strict:
             datacards[faction]["Units"] = [unit for unit in datacards[faction]["Units"] if all_keywords_shared(unit["Keywords"], searched_keywords)]
